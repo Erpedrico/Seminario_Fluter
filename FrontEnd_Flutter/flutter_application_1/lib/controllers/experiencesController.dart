e86b90198658a0598ad8425142eee7ec2264a3d0
juliaroquet <115218449+juliaroquet@users.noreply.github.com>
2024-11-16 01:17:14 +0100

    Update experiencesController.dart

--- FrontEnd_Flutter/flutter_application_1/lib/controllers/experiencesController.dart
+++ FrontEnd_Flutter/flutter_application_1/lib/controllers/experiencesController.dart
@@ -2,20 +2,18 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:flutter_application_1/models/experienceModel.dart';
 import 'package:flutter_application_1/services/experience.dart';
-
 class ExperienceController extends GetxController {
   final ExperienceService experienceService = Get.put(ExperienceService());
+  // Controladores de texto para la UI
   final TextEditingController ownerController = TextEditingController();
   final TextEditingController participantsController = TextEditingController();
   final TextEditingController descriptionController = TextEditingController();
-
-  
+  // Variables reactivas para la UI
   var isLoading = false.obs;
   var errorMessage = ''.obs;
-
-  // crear una nueva experiencia
+  // Método para crear una nueva experiencia
   void createExperience() async {
- 
+    // Validación de campos
     if (ownerController.text.isEmpty ||
         participantsController.text.isEmpty ||
         descriptionController.text.isEmpty) {
@@ -23,22 +21,20 @@ class ExperienceController extends GetxController {
           snackPosition: SnackPosition.BOTTOM);
       return;
     }
-
     // Crear la instancia de ExperienceModel
     final newExperience = ExperienceModel(
       owner: ownerController.text,
       participants: participantsController.text,
       description: descriptionController.text,
     );
-
-    
+    // Iniciar el proceso de creación
     isLoading.value = true;
     errorMessage.value = '';
-
     try {
+      // Llamada al servicio para crear la experiencia
       final statusCode = await experienceService.createExperience(newExperience);
-
       if (statusCode == 201) {
+        // Manejo de respuesta exitosa
         Get.snackbar('Éxito', 'Experiencia creada con éxito');
         Get.toNamed('/experiencies');
       } else {
